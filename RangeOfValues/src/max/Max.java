d34f08aca195ad0474e32913210a3b4dc6671b5d
Evelina-M <162884132+Evelina-M@users.noreply.github.com>
2024-07-05 12:09:23 +0300

    Adding Files

---
+++ RangeOfValues/src/max/Max.java
@@ -0,0 +1,65 @@
+package max;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.*;
+
+public class Max {
+
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
+        String[] texts = new String[25];
+        for (int i = 0; i < texts.length; i++) {
+            texts[i] = generateText("aab", 30_000);
+        }
+
+        ExecutorService executor = Executors.newFixedThreadPool(texts.length);
+        List<Future<Integer>> futures = new ArrayList<>();
+
+        long startTs = System.currentTimeMillis(); // start time
+
+        for (String text : texts) {
+            Callable<Integer> callable = () -> {
+                int maxSize = 0;
+                for (int i = 0; i < text.length(); i++) {
+                    for (int j = 0; j < text.length(); j++) {
+                        if (i >= j) {
+                            continue;
+                        }
+                        boolean bFound = false;
+                        for (int k = i; k < j; k++) {
+                            if (text.charAt(k) == 'b') {
+                                bFound = true;
+                                break;
+                            }
+                        }
+                        if (!bFound && maxSize < j - i) {
+                            maxSize = j - i;
+                        }
+                    }
+                }
+                return maxSize;
+            };
+            futures.add(executor.submit(callable));
+        }
+
+        int max = 0;
+        for (Future<Integer> future : futures) {
+            max = Math.max(max, future.get());
+        }
+
+        long endTs = System.currentTimeMillis(); // end time
+
+        System.out.println("Max size: " + max);
+        System.out.println("Time: " + (endTs - startTs) + "ms");
+    }
+
+    public static String generateText(String letters, int length) {
+        Random random = new Random();
+        StringBuilder text = new StringBuilder();
+        for (int i = 0; i < length; i++) {
+            text.append(letters.charAt(random.nextInt(letters.length())));
+        }
+        return text.toString();
+    }
+}
